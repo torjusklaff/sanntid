6c3dfad14f21a089ff30b7a1a26b60a0a44ae38e
torjusklaff <torjus.klafstad@gmail.com>
2017-02-03 11:56:34 +0100

    Update driver.go

--- project/driver/driver.go
+++ project/driver/driver.go
@@ -1,90 +1,81 @@
-package Driver  // where "driver" is the folder that contains io.go, io.c, io.h, channels.go, channels.h and driver.go
+package driver // where "driver" is the folder that contains io.go, io.c, io.h, channels.go, channels.h and driver.go
 /*
 #cgo CFLAGS: -std=c11
 #cgo LDFLAGS: -lcomedi -lm
-#include "io.h"
+#include "elev.h"
 */
 import "C"
 
-
 const (
-	n_floors = int(C.N_FLOORS)
-	n_buttons = int(C.N_BUTTONS)
+	N_floors  = int(C.N_FLOORS)
+	N_buttons = int(C.N_BUTTONS)
 )
-
 
 type motor_direction int
 
 const (
 	dir_down motor_direction = -1
 	dir_stop motor_direction = 0
-	dir_up motor_direction = 1
+	dir_up   motor_direction = 1
 )
 
-
-type button_type int
+type Button_type int
 
 const (
-	buttoncall_down button_type = 1
-	buttoncall_up button_type = 0
-	buttoncall_internal button_type = 2
+	Buttoncall_down     Button_type = 1
+	Buttoncall_up       Button_type = 0
+	Buttoncall_internal Button_type = 2
 )
 
-
 type order_button struct {
-	Type button_type
+	Type  Button_type
 	Floor int
 }
 
-
-
-func set_motor_direction(dirn motor_direction){
+func Set_motor_direction(dirn motor_direction) {
 	C.elev_set_motor_direction(C.elev_motor_direction_t(dirn))
 }
 
-func set_button_lamp(button button_type, floor int, value int){
-	C.elev_set_button_lamp(C.elev_set_button_lamp(C.elev_button_type_t(button), C.int(floor), C.int(value)))
+func Set_button_lamp(button Button_type, floor int, value int) {
+	C.elev_set_button_lamp(C.elev_button_type_t(button), C.int(floor), C.int(value))
 }
 
-func set_floor_indicator(floor int){
+func Set_floor_indicator(floor int) {
 	C.elev_set_floor_indicator(C.int(floor))
 }
 
-func set_door_open(value int){
-	C.elev_set_door_open(C.int(value))
+func Get_door_open_lamp(value int) {
+	C.elev_set_door_open_lamp(C.int(value))
 }
 
-func get_button_signal(button button_type, floor int){
+func Get_button_signal(button Button_type, floor int) int {
 	return int(C.elev_get_button_signal(C.elev_button_type_t(button), C.int(floor)))
 }
 
-func get_floor_sensor_signal(){
+func Get_floor_sensor_signal() int {
 	return int(C.elev_get_floor_sensor_signal())
 }
 
-func clear_all_lamps(){
-	for floor := 0; floor < n_floors; floor++ {
-		if floor < n_floors-1 {
-			set_button_lamp(buttoncall_down, floor, 0)
+func Clear_all_lamps() {
+	for floor := 0; floor < N_floors; floor++ {
+		if floor < N_floors-1 {
+			Set_button_lamp(Buttoncall_down, floor, 0)
 		}
 		if floor > 0 {
-			set_button_lamp(buttoncall_up, floor, 0)
+			Set_button_lamp(Buttoncall_up, floor, 0)
 		}
-		set_button_lamp(buttoncall_internal, floor, 0)
+		Set_button_lamp(Buttoncall_internal, floor, 0)
 	}
 }
 
-
-
-
-func elev_init(){
+func Elev_init() {
 	C.elev_init()
-	clear_all_lamps()
+	Clear_all_lamps()
 
-	set_motor_direction(dir_down)
-	for get_floor_sensor_signal() == -1 {
+	Set_motor_direction(dir_down)
+	for Get_floor_sensor_signal() == -1 {
 	}
-	set_motor_direction(dir_stop)
+	Set_motor_direction(dir_stop)
 }
 
 
