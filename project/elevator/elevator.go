c739fc80c17b1c4305dc8ac040109ef0b955a70c
torjusklaff <torjus.klafstad@gmail.com>
2017-02-17 11:55:34 +0100

    Update elevator.go

--- project/elevator/elevator.go
+++ project/elevator/elevator.go
@@ -1,38 +1,38 @@
+package elevator
+
 import (
 	"../driver"
 )
 
-type elev_states int
+type Elev_states int
 
 const (
-	idle elev_states = iota
+	idle Elev_states = iota
 	door_open
 	moving
 )
 
-
 type Elevator struct {
-	last_floor int
-	current_direction motor_direction
-	queue [driver.N_floors]int
-	elevator_state elev_states
-	door_open_duration float
+	last_floor         int
+	current_direction  driver.Motor_direction
+	queue              [driver.N_floors]int
+	elevator_state     Elev_states
+	door_open_duration float64
 }
 
-var elev_data elevator_data
+var elev_data Elevator
 
-
-func get_elev_data(){
+func Get_elev_data() Elevator {
 	return elev_data
 }
 
-func set_elev_data(floor last_floor, direction motor_direction){
+func Set_elev_data(floor int, direction driver.Motor_direction) {
 	elev_data{last_floor: floor}
 	elev_data{motor_direction: direction}
 }
 
-
-func init() {
+func Init() {
 	elev_init()
 	set_elev_data(get_floor_sensor_signal(), dir_stop)
 }
+
