abb55c9560609e5116b7677b750c089248a9df0e
mariepauline <mariepsk@gmail.com>
2017-02-22 15:21:44 +0100

    Add files via upload

--- project/elevator/elevator.go
+++ project/elevator/elevator.go
@@ -26,13 +26,16 @@ func Get_elev_data() Elevator {
 	return elev_data
 }
 
-func Set_elev_data(floor int, direction driver.Motor_direction) {
+func Set_elev_data(floor int, direction driver.Motor_direction, queue [driver.N_floors]int, elevator_state def.Elev_states, door_open_duration float64) {
 	elev_data{last_floor: floor}
 	elev_data{motor_direction: direction}
+	elev_data{queue: queue}
+	elev_data{elevator_state: elevator_state}
+	elev_data{door_open_duration: door_open_duration}
+
 }
 
 func Init() {
 	elev_init()
 	set_elev_data(get_floor_sensor_signal(), dir_stop)
 }
-
