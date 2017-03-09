63fd48b5354d99cdb73ed122d414721032584e1b
mariepauline <mariepsk@gmail.com>
2017-03-09 05:53:28 +0100

    lagt inn mye rart

--- project/arbitrator/arbitrator.go
+++ project/arbitrator/arbitrator.go
@@ -3,28 +3,110 @@ package arbitrator
 import (
 	"math"
 	def "../definitions"
+	"strings"
 )
 
 var max_distance int = def.N_floors * def.N_buttons
 
-func Find_lowest_cost(IP_adresses []string, costs []float32) to_elevator {
-	lowest_cost := math.Inf(+1)
-	to_elevator string := "lol"
-	for i := 0; i<len(costs); i++{
-		if costs[i] < lowest_cost {
-			lowest_cost = costs[i]
-			to_elevator = IP_adresses[i]
-		}
-	}
-}
 
-func Cost_function(elevator def.Elevator, order def.Order_button) cost{
+func cost_function(elevator def.Elevator, order def.Order_button) cost{
 	difference := order.Floor - elevator.Last_floor
 	cost := math.Abs(difference) 
 	+ movement_penalty(elevator.State, elevator.Current_direction, difference) 
 	+ turn_penalty(elevator.State, elevator.Last_floor, elevator.Current_direction, order.Floor)
 	+ order_direction_penalty(elevator.Current_direction, order.Floor, order.Type)
 }
+
+func find_lowest_cost(costs [def.N_elevators]def.Cost) def.Cost {
+	for i := 0; i<len(costs)-1; i++{
+		if costs[i+1].Cost_value < costs[i].Cost_value {
+			temp := costs[i]
+			costs[i] = costs[i+1]
+			costs [i+1] = temp
+		}
+		if costs[0] == costs[1] {
+			if (split_IP(costs[0].Id) < split_IP(costs[1].Id)){
+				return list[0]
+			} else {
+				return list[1]
+			}
+		}
+	}
+	return list[0]
+}
+
+
+func Arbitrator_init(
+	e def.Elevator,
+	localIP string, 
+	new_order <-chan def.Order_button, 
+	assigned_new_order <- chan def.Order_button,
+	receive_cost <-chan def.Cost, 
+	send_cost chan<- def.Cost, 
+	number_of_connected_elevators <-chan int){
+
+	var n_elevators int
+
+
+	for {
+		select {
+			case elevators := <- number_of_connected_elevators:
+				n_elevators = elevators
+			case current_new_order := <- new_order:
+				current_cost := def.Cost{Cost: cost_function(e, current_new_order), Current_order: current_new_order, Id: localIP}
+				order_selection(assigned_new_order, receive_cost, n_elevators, current_cost, localIP)
+		}
+	}
+}
+
+// Bestemmer om current heis skal ta bestillingen eller ikke, sender da på assigned_new_order
+func order_selection(
+	assigned_new_order chan<- def.Order_button, 
+	receive_cost <- chan def.Cost, 
+	n_elevators int, 
+	current_cost def.Cost, 
+	localIP string){
+
+	var cost_list [def.N_elevators]def.Cost
+
+	for i := 0; i < def.N_elevators; i++ {
+		cost_list[i] := def.Cost{math.Inf(+1), current_cost.Current_order, current_cost.Id}
+	}
+
+	switch (n_elevators){
+	case 1:
+		cost_list[0] = cost
+	case 2:
+		new_cost := <-elev_receive_cost_value
+		cost_list[0] = cost
+		cost_list[1] = new_cost
+	case 3:
+		new_cost := <-elev_receive_cost_value
+		new_cost2 := <-elev_receive_cost_value
+		cost_list[0] = cost
+		cost_list[1] = new_cost
+		cost_list[2] = new_cost2
+	}
+
+	// regner ut laveste kost av de aktive heisene
+	lowest_cost := find_lowest_cost(cost_list)
+
+	// sender 
+	if lowest_cost.Id == localIP {
+		assigned_new_order <- cost.Current_order
+	}
+}
+
+
+
+
+
+
+func split_IP(IP string) string {
+	s := strings.Split(IP, ".")
+	return s[3]
+}
+
 
 
 func movement_penalty(state def.Elev_states, direction def.Motor_direction, difference int) penalty{
--- project/network/network.go
+++ project/network/network.go
@@ -4,6 +4,131 @@ import (
 	"./bcast"
 	"./localip"
 	"./peers"
+	"time"
+	"../def"
 )
 
+const (
+	peer_port = 15647
+	get_order_port = 16571
+	remove_order_port = 16572
+	get_cost_port = 16573
+	backup_port = 16574
+	broadcast_time = 1*time.Second
+)
 
+type Cost_msg struct {
+	Adsress string
+	Data def.Cost
+}
+
+type Order_msg struct {
+	Adsress string
+	Data def.Order_button
+}
+
+
+// Setter opp alle channels og funksjoner i en felles initialisering
+func Network_init(
+	n_elevators chan<- int, 
+	receive_cost chan<- def.Cost,
+	receive_new_order chan<- def.Order_button,
+	receive_remover_order chan<- def.Order_button,
+	send_cost chan<- def.Cost,
+	send_new_order chan<- def.Order_button,
+	send_remove_order chan<- def.Order_button){
+
+	id := Get_id()
+	go Peer_listener(id, n_elevators)
+	go Send_msg(id, send_cost, send_new_order,send_remove_order)
+	go Receive_msg(receive_cost, receive_new_order,receive_remover_order)
+}
+
+
+
+func Get_id() id {
+	var id string
+	flag.StringVar(&id, "id", "", "id of this peer")
+	flag.Parse()
+	localIP, err := localip.LocalIP()
+	if err != nil {
+		fmt.Println(err)
+		localIP = "DISCONNECTED"
+	}
+	id = fmt.Sprintf("peer-%s-%d", localIP, os.Getpid())
+}
+
+// Setter opp en peer-listener som sjekker etter updates på levende heiser
+func Peer_listener(id string, number_of_elevators chan<- int){
+	peerUpdateCh := make(chan peers.PeerUpdate)
+	peerTxEnable := make(chan bool)
+	go peers.Transmitter(peer_port, id, peerTxEnable)
+	go peers.Receiver(peer_port, peerUpdateCh)
+	for {
+		select {
+		case p := <-peerUpdateCh:
+			fmt.Printf("Peer update:\n")
+			fmt.Printf("  Peers:    %q\n", p.Peers)
+			fmt.Printf("  New:      %q\n", p.New)
+			fmt.Printf("  Lost:     %q\n", p.Lost)
+			number_of_peers <- len(p.Peers)
+		}
+	}
+}
+
+// Setter opp channels for broadcast og sender det som kommer inn på input-channelsene 
+// se main fra network-module gitt på github
+func Send_msg(
+	localIP string, 
+	send_cost <-chan def.Cost, 
+	send_new_order <-chan def.Order_button,
+	send_remove_order <-chan def.Order_button){
+
+	bcast_send_cost := make(chan Cost_msg)
+	bcast_send_new_order := make(chan Order_msg)
+	bcast_send_remove_order := make(chan Order_msg)
+
+	go bcast.Transmitter(get_cost_port, bcast_send_cost)
+	go bcast.Transmitter(get_order_port, bcast_send_new_order)
+	go bcast.Transmitter(remove_order_port, bcast_send_remove_order)
+
+	for {
+		select {
+		case msg := <-send_cost:
+			sending := Cost_msg{Address: localIP, Data: msg}
+			bcast_send_cost <- sending
+		case msg := <-send_new_order:
+			sending := Order_msg{Address: localIP, Data: msg}
+			bcast_send_cost <- sending
+		case msg := <-send_remove_order:
+			sending := Order_msg{Address: localIP, Data: msg}
+			bcast_send_cost <- sending
+		}
+	}
+}
+
+// Setter opp channels som lytter etter msg fra Send_msg()		(se main fra network-modul)
+func Receive_msg(
+	receive_cost chan<- def.Cost, 
+	receive_new_order chan<- def.Order_button,
+	receive_remover_order chan<- def.Order_button){
+
+	bcast_receive_cost := make(chan Cost_msg)
+	bcast_receive_new_order := make(chan Order_msg)
+	bcast_receive_remove_order := make(chan Order_msg)
+
+	go bcast.Receiver(get_cost_port, bcast_receive_cost)
+	go bcast.Receiver(get_order_port, bcast_receive_new_order)
+	go bcast.Receiver(remove_order_port, bcast_receive_remove_order)
+
+	for {
+		select {
+		case msg := <-bcast_receive_cost:
+			receive_cost <_ msg.Data
+		case msg := <-bcast_receive_new_order:
+			receive_new_order <_ msg.Data
+		case msg := <-bcast_receive_remover_order:
+			receive_remover_order <_ msg.Data
+		}
+	}	
+}
